6024457d42467dad516a7f303aa41a7abeeb26e4
Daniel Field <n9932496@qut.edu.au>
2018-05-03 00:57:51 +1000

    Add blank MainPanel.java in gui package

---
+++ src/gui/MainPanel.java
@@ -0,0 +1,14 @@
+package gui;
+
+import javax.swing.JPanel;
+
+/**
+ * This is the main panel. All of the other panels go inside this panel.
+ * 
+ * @author Daniel Field
+ * @author Allen Basic
+ *
+ */
+public class MainPanel extends JPanel {
+	
+}
