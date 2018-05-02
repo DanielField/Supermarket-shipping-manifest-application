fe6a5774f92f3b9655d3e613f7e3585f14e14880
Daniel Field <n9932496@qut.edu.au>
2018-05-02 18:48:01 +1000

    Add programme entry point

---
+++ src/supermart/Supermart.java
@@ -0,0 +1,24 @@
+/**
+ * 
+ */
+package supermart;
+
+/**
+ * The primary class for the application. This is responsible for setting everything up.
+ * 
+ * @author Daniel Field
+ * @author Allen Basic
+ */
+public class Supermart {
+
+	/**
+	 * Programme's entry point.
+	 * 
+	 * @param args Command line arguments.
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
