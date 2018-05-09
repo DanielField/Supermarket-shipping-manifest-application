ab1d71ac2ff3c1136e8a02970725ebda99c9b08f
Daniel Field <n9932496@qut.edu.au>
2018-05-09 18:57:13 +1000

    Created GUI

--- src/gui/MainPanel.java
+++ src/gui/MainPanel.java
@@ -2,6 +2,8 @@ package gui;
 
 import javax.swing.JPanel;
 
+import supermart.Utils;
+
 /**
  * This is the main panel. All of the other panels go inside this panel.
  * 
@@ -10,5 +12,17 @@ import javax.swing.JPanel;
  *
  */
 public class MainPanel extends JPanel {
+	public MainPanel() {
+		initialisePanels();
+	}
 	
+	private void initialisePanels() {
+		MenuPanel menu = new MenuPanel();
+		ContentPanel content = new ContentPanel();
+		InfoPanel info = new InfoPanel();
+		
+		setLayout(null);
+		
+		Utils.addComponents(this, menu, content, info);
+	}
 }
---
+++ src/gui/MenuPanel.java
@@ -0,0 +1,25 @@
+/**
+ * 
+ */
+package gui;
+
+import java.awt.Color;
+import java.awt.Container;
+
+import javax.swing.JPanel;
+
+import supermart.Supermart;
+import supermart.Utils;
+
+/**
+ * @author Daniel Field
+ * @author Allen Basic
+ *
+ */
+public class MenuPanel extends JPanel {
+	public MenuPanel() {
+		setLayout(null);
+		setSize(120, Supermart.DEFAULT_H);
+		setBackground(Color.blue);
+	}
+}
