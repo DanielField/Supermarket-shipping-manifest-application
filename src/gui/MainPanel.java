a7c275e3ce68add2997176b008296b1ae7e2dba8
Daniel Field <n9932496@qut.edu.au>
2018-05-20 01:09:09 +1000

    Buttons and labels on GUI; deleted all JPanels except MainPanel

--- src/gui/MainPanel.java
+++ src/gui/MainPanel.java
@@ -1,8 +1,11 @@
 package gui;
 
-import javax.swing.JPanel;
+import java.awt.Color;
 
-import supermart.Utils;
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.SpringLayout;
 
 /**
  * This is the main panel. All of the other panels go inside this panel.
@@ -12,17 +15,44 @@ import supermart.Utils;
  *
  */
 public class MainPanel extends JPanel {
+	private static final long serialVersionUID = 1L;
+
+	private SpringLayout layout;
+	
 	public MainPanel() {
-		initialisePanels();
+		layout = new SpringLayout();
+		setLayout(layout);
+
+		InitialiseButtons();
+		InitialiseLabels();
 	}
 	
-	private void initialisePanels() {
-		MenuPanel menu = new MenuPanel();
-		ContentPanel content = new ContentPanel();
-		InfoPanel info = new InfoPanel();
+//	private void initialisePanels() {
+//		MenuPanel menu = new MenuPanel();
+//		ContentPanel content = new ContentPanel();
+//		InfoPanel info = new InfoPanel();
+//		
+//		setLayout(null);
+//		setBackground(Color.BLACK);
+//		
+//		Components.addComponents(this, menu, content, info);
+//	}
+	
+	private void InitialiseButtons() {
+		JButton btnStoreInfo, btnInventory, btnManifest, btnSalesLog;
 		
-		setLayout(null);
+		btnStoreInfo = Components.CreateButton(this, layout, "Store Info", 10, 10);
+		btnInventory = Components.CreateButton(this, layout, "Inventory", 10, 40);
+		btnManifest = Components.CreateButton(this, layout, "Manifest", 10, 70);
+		btnSalesLog = Components.CreateButton(this, layout, "Sales Log", 10, 100);
 		
-		Utils.addComponents(this, menu, content, info);
+		Components.addComponents(this, btnStoreInfo, btnInventory, btnManifest, btnSalesLog);
+	}
+	
+	private void InitialiseLabels() {
+		JLabel lblName = Components.CreateLabel(this, layout, "Store name: Supermart", 150, 10);
+		JLabel lblCapital = Components.CreateLabel(this, layout, "Store capital: $0", 150, 40);
+		
+		Components.addComponents(this, lblName, lblCapital);
 	}
 }
