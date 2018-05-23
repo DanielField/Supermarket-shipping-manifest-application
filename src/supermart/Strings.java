579e04197977a946eb278055869245ac17437f45
Daniel Field <n9932496@qut.edu.au>
2018-05-23 19:09:22 +1000

    Export manifest; import manifest improved

--- src/supermart/Strings.java
+++ src/supermart/Strings.java
@@ -12,4 +12,5 @@ package supermart;
 public class Strings {
 	public static final String ITEM_PROPERTIES_CSV = "item_properties.csv";
 	public static final String STORE_INFO_CSV = "store.csv";
+	public static final String MANIFEST_CSV = "manifest.csv";
 }
