425c7910e14c69fab85f6c1d19fe502b9d32dc4d
Allen <allen.basic@connect.qut.edu.au>
2018-05-24 23:38:09 +1000

    Added test cases for Item and Stock

--- src/tests/ItemTests.java
+++ src/tests/ItemTests.java
@@ -4,6 +4,8 @@
 package tests;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotEquals;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -40,6 +42,7 @@ public class ItemTests {
 		ordinary = new OrdinaryItem(null, 0, 0, 0, 0);
 	}
 	
+	//PerishableItem name tests
 	@Test
 	public void testPerishableName() throws InvalidItemException {
 		perishable.setName("Chicken");
@@ -50,6 +53,412 @@ public class ItemTests {
 	
 	@Test(expected=InvalidItemException.class)
 	public void testPerishableNameEmpty() throws InvalidItemException {
-		perishable.setName("");
+		perishable.setName("");		
 	}
+	
+	@Test
+	public void testPerishableNameNumber() throws InvalidItemException {
+		perishable.setName("42");
+		String name = perishable.getName();
+		
+		assertEquals("42", name);
+	}
+	
+	@Test
+	public void testLongPerishableName() throws InvalidItemException {
+		perishable.setName("Verylongcerealbrandwithaverylongname"); 
+		String name = perishable.getName();
+		
+		assertEquals("Verylongcerealbrandwithaverylongname", name);
+	}
+	
+	@Test
+	public void testShortPerishableName() throws InvalidItemException {
+		perishable.setName("x");
+		String name = perishable.getName();
+		
+		assertEquals("x", name);
+	}
+	//end PerishableItem name tests
+	
+	//PerishableItem manufacturtingCost tests
+	@Test
+	public void testPerishableItemManufacturingCost() throws InvalidItemException {
+		perishable.setManufacturingCost(54.85);
+		double cost = perishable.getManufacturingCost();
+		
+		assertEquals(54.85, cost, 0.001);		
+	}
+	
+	@Test
+	public void testPerishableItemManufacturingCostWrong() throws InvalidItemException {
+		perishable.setManufacturingCost(43);
+		double cost = perishable.getManufacturingCost();
+		
+		assertNotEquals(22, cost);
+	}
+	
+	@Test
+	public void testPerishableItemManufacturingCostZero() throws InvalidItemException {
+		perishable.setManufacturingCost(0);
+		double cost = perishable.getManufacturingCost();
+		
+		assertEquals(0, cost, 0.000);
+	}
+	
+	@Test(expected=InvalidItemException.class)
+	public void testPerishableItemManufacturingCostNegative() throws InvalidItemException {
+		perishable.setManufacturingCost(-42);		
+	}
+	
+	@Test
+	public void testPerishableItemManufacturingCostBig() throws InvalidItemException {
+		perishable.setManufacturingCost(1000000);
+		double cost = perishable.getManufacturingCost();
+		
+		assertEquals(1000000, cost, 0.001);
+	}	
+	//end PerishableItem manufacturtingCost tests
+	
+	
+	//PerishableItem SellPrice tests
+	@Test
+	public void testPerishableItemSellPrice() throws InvalidItemException {
+		perishable.setSellPrice(78.2);
+		double price = perishable.getSellPrice();
+		
+		assertEquals(78.2, price, 0.001);							
+	}
+	
+	@Test
+	public void testPerishableItemSellPriceWrong() throws InvalidItemException {
+		perishable.setSellPrice(3);
+		double price = perishable.getSellPrice();
+		
+		assertNotEquals(1, price, 0.001);
+	}
+	
+	@Test
+	public void testPerishableItemSellPriceZero() throws InvalidItemException {
+		perishable.setSellPrice(0);
+		double price = perishable.getSellPrice();
+		
+		assertEquals(0, price, 0.000);	
+	}
+	
+	@Test
+	public void testPerishableItemSellPriceBig() throws InvalidItemException {
+		perishable.setSellPrice(1000000);
+		double price = perishable.getSellPrice();
+		
+		assertEquals(1000000, price, 0.001);
+	}
+	
+	@Test(expected=InvalidItemException.class)
+	public void testPerishableItemSellPriceNegative() throws InvalidItemException {
+		perishable.setSellPrice(-87.22);
+	}
+	//end PerishableItem SellPrice tests
+	
+	//PerishableItem reorderPoint tests
+	@Test
+	public void testPerishableItemreorderPoint() throws InvalidItemException {
+		perishable.setReorderPoint(20);
+		int point = perishable.getReorderPoint();
+		
+		assertEquals(20, point);
+	}
+	
+	@Test
+	public void testPerishableItemreorderPointWrong() throws InvalidItemException {
+		perishable.setReorderPoint(43);
+		int point = perishable.getReorderPoint();
+		
+		assertNotEquals(44, point);
+	}
+	
+	@Test(expected=InvalidItemException.class)
+	public void testPerishableItemreorderPointNegative() throws InvalidItemException {
+		perishable.setReorderPoint(-2);
+	}
+	
+	@Test
+	public void testPerishableItemreorderPointZero() throws InvalidItemException {
+		perishable.setReorderPoint(0);
+		int point = perishable.getReorderPoint();
+		
+		assertEquals(0, point, 0.000);
+	}
+	
+	@Test
+	public void testPerishableItemreorderPointBig() throws InvalidItemException {
+		perishable.setReorderPoint(1000000);
+		int point = perishable.getReorderPoint();
+		
+		assertEquals(1000000, point, 0.001);
+	}
+	//end PerishableItem reorderPoint tests
+	
+	//PerishableItem reorderAmount tests
+	@Test
+	public void testPerishableItemreorderAmount() throws InvalidItemException {
+		perishable.setReorderAmount(400);
+		int amount = perishable.getReorderAmount();
+		
+		assertEquals(400, amount);
+	}
+	
+	@Test
+	public void testPerishableItemreorderAmountWrong() throws InvalidItemException {
+		perishable.setReorderAmount(523);
+		int amount = perishable.getReorderAmount();
+		
+		assertNotEquals(323, amount);
+	}
+	
+	@Test(expected=InvalidItemException.class)
+	public void testPerishableItemreorderAmountNegative() throws InvalidItemException {
+		perishable.setReorderAmount(-52);
+	}
+	
+	@Test
+	public void testPerishableItemreorderAmountZero() throws InvalidItemException {
+		perishable.setReorderAmount(0);
+		int amount = perishable.getReorderAmount();
+		
+		assertEquals(0, amount, 0.000);						
+	}
+	
+	@Test
+	public void testPerishableItemreorderAmountBig() throws InvalidItemException {
+		perishable.setReorderAmount(1000000);
+		int amount = perishable.getReorderAmount();
+		
+		assertEquals(1000000, amount, 0.001);
+	}
+	//end PerishableItem reorderAmount tests
+	
+	//PerishableItem Temperature Tests
+	@Test
+	public void testPerishableItemTemperature() throws InvalidItemException {	
+		((PerishableItem) perishable).setTemperature(-14);
+		double temp = ((PerishableItem) perishable).getTemperature();
+		
+		assertEquals(-14,temp, 0.000);		
+	}
+	
+	@Test (expected=InvalidItemException.class)
+	public void testPerishableItemTemperatureTooCold() throws InvalidItemException {
+		((PerishableItem) perishable).setTemperature(-10000);
+	}
+	
+	@Test (expected=InvalidItemException.class)
+	public void testPerishableItemTemperatureTooHot() throws InvalidItemException {
+		((PerishableItem) perishable).setTemperature(100);
+	}	
+	//End PerishableItem Temperature Tests
+	
+	//OrdinaryItem
+	
+	//OrdinaryItem name tests
+		@Test
+		public void testOrdinaryName() throws InvalidItemException {
+			ordinary.setName("Chicken");
+			String name = ordinary.getName();
+			
+			assertEquals("Chicken", name);
+		}
+		
+		@Test(expected=InvalidItemException.class)
+		public void testOrdinaryNameEmpty() throws InvalidItemException {
+			ordinary.setName("");		
+		}
+		
+		@Test
+		public void testOrdinaryNameNumber() throws InvalidItemException {
+			ordinary.setName("42");
+			String name = ordinary.getName();
+			
+			assertEquals("42", name);
+		}
+		
+		@Test
+		public void testLongOrdinaryName() throws InvalidItemException {
+			ordinary.setName("Verylongcerealbrandwithaverylongname"); 
+			String name = ordinary.getName();
+			
+			assertEquals("Verylongcerealbrandwithaverylongname", name);
+		}
+		
+		@Test
+		public void testShortOrdinaryName() throws InvalidItemException {
+			ordinary.setName("x");
+			String name = ordinary.getName();
+			
+			assertEquals("x", name);
+		}
+		//end OrdinaryItem name tests
+		
+		//OrdinaryItem manufacturtingCost tests
+		@Test
+		public void testOrdinaryItemManufacturingCost() throws InvalidItemException {
+			ordinary.setManufacturingCost(54.85);
+			double cost = ordinary.getManufacturingCost();
+			
+			assertEquals(54.85, cost, 0.001);		
+		}
+		
+		@Test
+		public void testOrdinaryItemManufacturingCostWrong() throws InvalidItemException {
+			ordinary.setManufacturingCost(43);
+			double cost = ordinary.getManufacturingCost();
+			
+			assertNotEquals(22, cost);
+		}
+		
+		@Test
+		public void testOrdinaryItemManufacturingCostZero() throws InvalidItemException {
+			ordinary.setManufacturingCost(0);
+			double cost = ordinary.getManufacturingCost();
+			
+			assertEquals(0, cost, 0.000);
+		}
+		
+		@Test(expected=InvalidItemException.class)
+		public void testOrdinaryItemManufacturingCostNegative() throws InvalidItemException {
+			ordinary.setManufacturingCost(-42);		
+		}
+		
+		@Test
+		public void testOrdinaryItemManufacturingCostBig() throws InvalidItemException {
+			ordinary.setManufacturingCost(1000000);
+			double cost = ordinary.getManufacturingCost();
+			
+			assertEquals(1000000, cost, 0.001);
+		}	
+		//end OrdinaryItem manufacturtingCost tests
+		
+		
+		//OrdinaryItem SellPrice tests
+		@Test
+		public void testOrdinaryItemSellPrice() throws InvalidItemException {
+			ordinary.setSellPrice(78.2);
+			double price = ordinary.getSellPrice();
+			
+			assertEquals(78.2, price, 0.001);							
+		}
+		
+		@Test
+		public void testOrdinaryItemSellPriceWrong() throws InvalidItemException {
+			ordinary.setSellPrice(3);
+			double price = ordinary.getSellPrice();
+			
+			assertNotEquals(1, price, 0.001);
+		}
+		
+		@Test
+		public void testOrdinaryItemSellPriceZero() throws InvalidItemException {
+			ordinary.setSellPrice(0);
+			double price = ordinary.getSellPrice();
+			
+			assertEquals(0, price, 0.000);	
+		}
+		
+		@Test
+		public void testOrdinaryItemSellPriceBig() throws InvalidItemException {
+			ordinary.setSellPrice(1000000);
+			double price = ordinary.getSellPrice();
+			
+			assertEquals(1000000, price, 0.001);
+		}
+		
+		@Test(expected=InvalidItemException.class)
+		public void testOrdinaryItemSellPriceNegative() throws InvalidItemException {
+			ordinary.setSellPrice(-87.22);
+		}
+		//end OrdinaryItem SellPrice tests
+		
+		//OrdinaryItem reorderPoint tests
+		@Test
+		public void testOrdinaryItemreorderPoint() throws InvalidItemException {
+			ordinary.setReorderPoint(20);
+			int point = ordinary.getReorderPoint();
+			
+			assertEquals(20, point);
+		}
+		
+		@Test
+		public void testOrdinaryItemreorderPointWrong() throws InvalidItemException {
+			ordinary.setReorderPoint(43);
+			int point = ordinary.getReorderPoint();
+			
+			assertNotEquals(44, point);
+		}
+		
+		@Test(expected=InvalidItemException.class)
+		public void testOrdinaryItemreorderPointNegative() throws InvalidItemException {
+			ordinary.setReorderPoint(-2);
+		}
+		
+		@Test
+		public void testOrdinaryItemreorderPointZero() throws InvalidItemException {
+			ordinary.setReorderPoint(0);
+			int point = ordinary.getReorderPoint();
+			
+			assertEquals(0, point, 0.000);
+		}
+		
+		@Test
+		public void testOrdinaryItemreorderPointBig() throws InvalidItemException {
+			ordinary.setReorderPoint(1000000);
+			int point = ordinary.getReorderPoint();
+			
+			assertEquals(1000000, point, 0.001);
+		}
+		//end OrdinaryItem reorderPoint tests
+		
+		//OrdinaryItem reorderAmount tests
+		@Test
+		public void testOrdinaryItemreorderAmount() throws InvalidItemException {
+			ordinary.setReorderAmount(400);
+			int amount = ordinary.getReorderAmount();
+			
+			assertEquals(400, amount);
+		}
+		
+		@Test
+		public void testOrdinaryItemreorderAmountWrong() throws InvalidItemException {
+			ordinary.setReorderAmount(523);
+			int amount = ordinary.getReorderAmount();
+			
+			assertNotEquals(323, amount);
+		}
+		
+		@Test(expected=InvalidItemException.class)
+		public void testOrdinaryItemreorderAmountNegative() throws InvalidItemException {
+			ordinary.setReorderAmount(-52);
+		}
+		
+		@Test
+		public void testOrdinaryItemreorderAmountZero() throws InvalidItemException {
+			ordinary.setReorderAmount(0);
+			int amount = ordinary.getReorderAmount();
+			
+			assertEquals(0, amount, 0.000);						
+		}
+		
+		@Test
+		public void testOrdinaryItemreorderAmountBig() throws InvalidItemException {
+			ordinary.setReorderAmount(1000000);
+			int amount = ordinary.getReorderAmount();
+			
+			assertEquals(1000000, amount, 0.001);
+		}
+		//end OrdinaryItem reorderAmount tests
+
+	
+	
+	
+	
+	
 }
--- src/tests/StockTests.java
+++ src/tests/StockTests.java
@@ -13,6 +13,7 @@ import exception.StockException;
 import stock.Item;
 import stock.ItemStock;
 import stock.OrdinaryItem;
+import stock.PerishableItem;
 import stock.Stock;
 
 /**
@@ -38,7 +39,7 @@ public class StockTests {
 	 * @throws InvalidItemException 
 	 */
 	@Test
-	public void testAddOrdinary() throws InvalidItemException {
+	public void testAddOrdinary() throws StockException, InvalidItemException {
 		assertEquals(0, stock.stockTotal());
 		
 		Item apple = new OrdinaryItem(null, 0, 0, 0, 0);
@@ -48,17 +49,263 @@ public class StockTests {
 	}
 	
 	/**
-	 * Retrieves the ItemStock object in order to use the ID to check whether increaseQuantity works correctly.
-	 * 
+	 * Expected result: The icecream object will be added, with a quantity of 300.
+	 * @throws InvalidItemException 
+	 */
+	@Test
+	public void testAddPerishable() throws StockException, InvalidItemException {
+		assertEquals(0, stock.stockTotal());
+		
+		Item icecream = new PerishableItem(null, 0, 0, 0, 0, -12);
+		stock.addNewItem(icecream, 40);
+		
+		assertEquals(40, stock.stockTotal());
+	}
+	
+	/**
+	 * Expected result: The apple object will be removed from the stock
+	 * @throws StockException
+	 * @throws InvalidItemException
+	 */
+	@Test
+	public void testRemoveOrdinary() throws StockException, InvalidItemException {
+		Item apple = new OrdinaryItem(null, 0, 0, 0, 0);
+		ItemStock is = stock.addNewItem(apple, 300);
+		
+		assertTrue(stock.contains(is));
+		
+		stock.removeItem(is.getItemID());	
+		
+		assertFalse(stock.contains(is));			
+	}
+	
+	/**
+	 * Expected result: The ice cream object will be removed from the stock
+	 * @throws StockException
+	 * @throws InvalidItemException
+	 */
+	@Test
+	public void testRemovePerishable() throws StockException, InvalidItemException {
+		Item icecream = new PerishableItem(null, 0, 0, 0, 0, -12);
+		ItemStock is = stock.addNewItem(icecream, 300);
+		
+		assertTrue(stock.contains(is));
+		
+		stock.removeItem(is.getItemID());	
+		
+		assertFalse(stock.contains(is));			
+	}
+	
+	/**
+	 * Check the quantity of the item to make sure getQuantity method is correct
+	 * @throws StockException
+	 * @throws InvalidItemException
+	 */
+	@Test
+	public void testOrdinaryQuantity() throws StockException, InvalidItemException {
+		Item apple = new OrdinaryItem(null, 0, 0, 0, 0);
+		ItemStock is = stock.addNewItem(apple, 300);		
+		int stock = is.getQuantity();
+		
+		assertEquals(300, stock);
+	}
+	
+	/**
+	 * Check the quantity of the item to make sure getQuantity method is correct
+	 * @throws StockException
+	 * @throws InvalidItemException
+	 */
+	@Test
+	public void testPerishableQuantity() throws StockException, InvalidItemException {
+		Item icecream = new PerishableItem(null, 0, 0, 0, 0, -12);
+		ItemStock is = stock.addNewItem(icecream, 40);		
+		int quantity = is.getQuantity();
+		
+		assertEquals(40, quantity);
+	}	
+	
+	/**
+	 * Testing to see if uniqueItemCount Method works
+	 * Expected Result: Return two unique items
+	 * @throws StockException
+	 * @throws InvalidItemException
+	 */
+	@Test
+	public void testUniqueItemCount() throws StockException, InvalidItemException {
+		Item icecream = new PerishableItem(null, 0, 0, 0, 0, -12);
+		Item apple = new OrdinaryItem(null, 0, 0, 0, 0);
+		
+		stock.addNewItem(icecream, 40);
+		stock.addNewItem(apple, 300);
+		
+		int count  = stock.uniqueItemCount();
+		
+		assertEquals(2, count);	
+	}
+	
+	/**
+	 * test to see if the sum of all quantities is as expected
+	 * by doing this test we see that the stockTotal method in Stock.java is working
+	 * @throws StockException
+	 * @throws InvalidItemException
+	 */
+	@Test
+	public void testStockTotal() throws StockException, InvalidItemException {
+		Item icecream = new PerishableItem(null, 0, 0, 0, 0, -12);
+		Item apple = new OrdinaryItem(null, 0, 0, 0, 0);
+		
+		stock.addNewItem(icecream, 40);
+		stock.addNewItem(apple, 300);
+		
+		int count = stock.stockTotal();
+		
+		assertEquals(340, count);
+	}
+	
+	/**
+	 * Testing the method of retrieving an item through ID
+	 * @throws StockException
+	 * @throws InvalidItemException
+	 */
+	@Test
+	public void testGetOrdinaryItemByID() throws StockException, InvalidItemException {
+		Item apple = new OrdinaryItem(null, 0, 0, 0, 0);
+		ItemStock is = stock.addNewItem(apple, 300);	
+		
+		int id = is.getItemID();
+		ItemStock itemstock = stock.getItemStock(id);
+		
+		assertEquals(is, itemstock);
+	}
+	/**
+	 * Testing the method of retrieving an item by its object
+	 * @throws StockException
+	 * @throws InvalidItemException
+	 */
+	@Test
+	public void testGetOrdinaryItemByObject() throws StockException, InvalidItemException {
+		Item apple = new OrdinaryItem(null, 0, 0, 0, 0);
+		ItemStock is = stock.addNewItem(apple, 300);	
+		
+		Item item = is.getItem();
+		ItemStock itemstock = stock.getItemStock(item);
+		
+		assertEquals(is, itemstock);
+	}
+	
+	/**
+	 * Testing the method of retrieving an item by its item name
+	 * @throws StockException
+	 * @throws InvalidItemException
+	 */
+	@Test
+	public void testGetOrdinaryItemByName() throws StockException, InvalidItemException {
+		Item apple = new OrdinaryItem(null, 0, 0, 0, 0);
+		ItemStock is = stock.addNewItem(apple, 300);	
+		
+		String item = is.getItem().getName() ;
+		
+		assertEquals(item, "icecream");
+	}	
+	
+	//PerishableItem getItembyId, name and object tests
+	/**
+	 * Testing the method of retrieving an item through ID
+	 * @throws StockException
+	 * @throws InvalidItemException
+	 */
+	@Test
+	public void testGetPerishableItemByID() throws StockException, InvalidItemException {
+		Item icecream = new PerishableItem(null, 0, 0, 0, 0, -12);
+		ItemStock is = stock.addNewItem(icecream, 40);	
+		
+		int id = is.getItemID();
+		ItemStock itemstock = stock.getItemStock(id);
+		
+		assertEquals(is, itemstock);
+	}
+	/**
+	 * Testing the method of retrieving an item by its object
+	 * @throws StockException
+	 * @throws InvalidItemException
+	 */
+	@Test
+	public void testGetPerishableItemByObject() throws StockException, InvalidItemException {
+		Item icecream = new PerishableItem(null, 0, 0, 0, 0, -12);
+		ItemStock is = stock.addNewItem(icecream, 40);	
+		
+		Item item = is.getItem();
+		ItemStock itemstock = stock.getItemStock(item);
+		
+		assertEquals(is, itemstock);
+	}
+	
+	/**
+	 * Testing the method of retrieving an item by its item name
+	 * @throws StockException
+	 * @throws InvalidItemException
+	 */
+	@Test
+	public void testGetPerishableItemByName() throws StockException, InvalidItemException {
+		Item icecream = new PerishableItem("icecream", 0, 0, 0, 0, -12);
+		ItemStock is = stock.addNewItem(icecream, 40);	
+		
+		String item = is.getItem().getName() ;
+		
+		assertEquals(item, "icecream");
+	}	
+	//end PerishableItem getItembyId, name and object tests
+	
+	
+	/**
+	 * Expected result: Throw an exception since the object we are trying to add is null
+	 * @throws StockException
+	 * @throws InvalidItemException
+	 */
+	@Test (expected = StockException.class)
+	public void testAddNullOrdinary() throws StockException, InvalidItemException {
+		stock.addNewItem(null, 0);
+	}
+	
+	/**
+	 * Expected result: Throw an exception since the object we are trying to add is null
+	 * @throws StockException
+	 * @throws InvalidItemException
+	 */
+	@Test (expected = StockException.class)
+	public void testAddNullPerishable() throws StockException, InvalidItemException {
+		stock.addNewItem(null, 0);
+	}
+	
+	/**
+	 * Expected Result: Throw an exception for trying to add two of same objects
+	 * Increase Quantity method should be used to add more of an item
+	 * Add method used to add a new item to Stock
+	 * @throws StockException
 	 * @throws InvalidItemException 
 	 */
 	@Test(expected = InvalidItemException.class)
-	public void testAddTwoOfTheSameOrdinaryItem() throws InvalidItemException {
+	public void testAddTwoOfTheSameOrdinaryItem() throws StockException, InvalidItemException {
 		Item apple = new OrdinaryItem(null, 0, 0, 0, 0);
 		stock.addNewItem(apple, 300);
 		stock.addNewItem(apple, 300);
 	}
 	
+	/**
+	 * Expected Result: Throw an exception for trying to add two of same objects
+	 * Increase Quantity method should be used to add more of an item
+	 * Add method used to add a new item to Stock
+	 * @throws StockException
+	 * @throws InvalidItemException 
+	 */
+	@Test(expected = InvalidItemException.class)
+	public void testAddTwoOfTheSamePerishableItem() throws StockException, InvalidItemException {
+		Item icecream = new PerishableItem(null, 0, 0, 0, 0, -12);
+		stock.addNewItem(icecream, 40);
+		stock.addNewItem(icecream, 40);
+	}
+	
+	//OrdinaryItem increaseQuantity tests
 	/**
 	 * Retrieves the ItemStock object in order to use the ID to check whether increaseQuantity works correctly.
 	 * 
@@ -74,6 +321,94 @@ public class StockTests {
 		assertEquals(320, stock.stockTotal());
 	}
 	
+	@Test
+	public void testOrdinaryIncreaseQuantityWrong() throws StockException, InvalidItemException {
+		Item apple = new OrdinaryItem(null, 0, 0, 0, 0);
+		ItemStock is = stock.addNewItem(apple, 300);
+		
+		stock.increaseQuantity(is.getItemID(), 20);
+		assertNotEquals(20, stock.stockTotal());
+	}
+	
+	@Test (expected = InvalidItemException.class)
+	public void testOrdinaryIncreaseQuantityNegative() throws StockException, InvalidItemException {
+		Item apple = new OrdinaryItem(null, 0, 0, 0, 0);
+		ItemStock is = stock.addNewItem(apple, 300);		
+		stock.increaseQuantity(is.getItemID(), -20);
+	}
+	
+	@Test
+	public void testOrdinaryIncreaseQuantityZero() throws StockException, InvalidItemException {
+		Item apple = new OrdinaryItem(null, 0, 0, 0, 0);
+		ItemStock is = stock.addNewItem(apple, 300);
+		
+		stock.increaseQuantity(is.getItemID(), 0);
+		assertEquals(300, stock.stockTotal());
+	}
+	
+	public void testOrdinaryIncreaseQuantityBig() throws StockException, InvalidItemException {
+		Item apple = new OrdinaryItem(null, 0, 0, 0, 0);
+		ItemStock is = stock.addNewItem(apple, 300);
+		
+		stock.increaseQuantity(is.getItemID(), 1000000);
+		assertEquals(1000300, stock.stockTotal());
+	}
+	
+	//end OrdinaryItem increaseQuantity tests
+		
+	//PerishableItem increaseQuantity tests
+	
+	/**
+	 * Retrieves the ItemStock object in order to use the ID to check whether increaseQuantity works correctly.
+	 * 
+	 * @throws StockException Throws exception if the test causes the quantity to be negative.
+	 * @throws InvalidItemException Throws if the item already exists.
+	 */
+	@Test
+	public void testPerishableIncreaseQuantity() throws StockException, InvalidItemException {
+		Item icecream = new PerishableItem(null, 0, 0, 0, 0, -12);
+		ItemStock is = stock.addNewItem(icecream, 40);
+		
+		stock.increaseQuantity(is.getItemID(), 200);
+		assertEquals(240, stock.stockTotal());
+	}
+	
+	@Test
+	public void testPerishableIncreaseQuantityWrong() throws StockException, InvalidItemException {
+		Item icecream = new PerishableItem(null, 0, 0, 0, 0, -12);
+		ItemStock is = stock.addNewItem(icecream, 40);
+		
+		stock.increaseQuantity(is.getItemID(), 20);
+		assertNotEquals(20, stock.stockTotal());
+	}
+	
+	@Test (expected = StockException.class)
+	public void testPerishableIncreaseQuantityNegative() throws StockException, InvalidItemException {
+		Item icecream = new PerishableItem(null, 0, 0, 0, 0, -12);
+		ItemStock is = stock.addNewItem(icecream, 40);		
+		stock.increaseQuantity(is.getItemID(), -20);
+	}
+	
+	@Test
+	public void testPerishableIncreaseQuantityZero() throws StockException, InvalidItemException {
+		Item icecream = new PerishableItem(null, 0, 0, 0, 0, -12);
+		ItemStock is = stock.addNewItem(icecream, 40);
+		
+		stock.increaseQuantity(is.getItemID(), 0);
+		assertEquals(40, stock.stockTotal());
+	}
+	
+	public void testPerishableIncreaseQuantityBig() throws StockException, InvalidItemException {
+		Item icecream = new PerishableItem(null, 0, 0, 0, 0, -12);
+		ItemStock is = stock.addNewItem(icecream, 40);
+		
+		stock.increaseQuantity(is.getItemID(), 1000000);
+		assertEquals(1000040, stock.stockTotal());
+	}
+	
+	//end perishableItem increaseQuantity tests
+	
+	//OrdinaryItem decreaseQuantity tests
 	/**
 	 * @throws StockException
 	 * @throws InvalidItemException
@@ -86,4 +421,86 @@ public class StockTests {
 		stock.decreaseQuantity(is.getItemID(), 200);
 		assertEquals(100, stock.stockTotal());
 	}
+		
+	@Test
+	public void testOrdinaryDecreaseQuantityWrong() throws StockException, InvalidItemException {
+		Item apple = new OrdinaryItem(null, 0, 0, 0, 0);
+		ItemStock is = stock.addNewItem(apple, 300);
+		
+		stock.decreaseQuantity(is.getItemID(), 20);
+		assertNotEquals(180, stock.stockTotal());
+	}
+	
+	@Test (expected = StockException.class)
+	public void testOrdinaryDecreaseQuantityNegative() throws StockException, InvalidItemException {
+		Item apple = new OrdinaryItem(null, 0, 0, 0, 0);
+		ItemStock is = stock.addNewItem(apple, 300);		
+		stock.decreaseQuantity(is.getItemID(), -20);
+	}
+	
+	@Test
+	public void testOrdinaryDecreaseQuantityZero() throws StockException, InvalidItemException {
+		Item apple = new OrdinaryItem(null, 0, 0, 0, 0);
+		ItemStock is = stock.addNewItem(apple, 300);
+		
+		stock.decreaseQuantity(is.getItemID(), 0);
+		assertEquals(300, stock.stockTotal());
+	}
+	
+	@Test (expected = StockException.class)
+	public void testOrdinaryDecreaseQuantityBig() throws StockException, InvalidItemException {
+		Item apple = new OrdinaryItem(null, 0, 0, 0, 0);
+		ItemStock is = stock.addNewItem(apple, 300);	
+		stock.decreaseQuantity(is.getItemID(), 1000000);		
+	}
+	
+	//end OrdinaryItem decreaseQuantity tests
+
+	//PerishableItem decreaseQuantity tests
+	
+	/**
+	 * @throws StockException
+	 * @throws InvalidItemException
+	 */
+	@Test
+	public void testPerishableDecreaseQuantity() throws StockException, InvalidItemException {
+		Item icecream = new PerishableItem(null, 0, 0, 0, 0, -12);
+		ItemStock is = stock.addNewItem(icecream, 40);
+		
+		stock.decreaseQuantity(is.getItemID(), 22);
+		assertEquals(18, stock.stockTotal());
+	}
+		
+	@Test
+	public void testPerishableDecreaseQuantityWrong() throws StockException, InvalidItemException {
+		Item icecream = new PerishableItem(null, 0, 0, 0, 0, -12);
+		ItemStock is = stock.addNewItem(icecream, 40);
+		
+		stock.decreaseQuantity(is.getItemID(), 20);
+		assertNotEquals(180, stock.stockTotal());
+	}
+	
+	@Test (expected = StockException.class)
+	public void testPerishableDecreaseQuantityNegative() throws StockException, InvalidItemException {
+		Item icecream = new PerishableItem(null, 0, 0, 0, 0, -12);
+		ItemStock is = stock.addNewItem(icecream, 40);		
+		stock.decreaseQuantity(is.getItemID(), -20);
+	}
+	
+	@Test
+	public void testPerishableDecreaseQuantityZero() throws StockException, InvalidItemException {
+		Item icecream = new PerishableItem(null, 0, 0, 0, 0, -12);
+		ItemStock is = stock.addNewItem(icecream, 40);
+		
+		stock.decreaseQuantity(is.getItemID(), 0);
+		assertEquals(40, stock.stockTotal());
+	}
+	
+	@Test (expected = StockException.class)
+	public void testPerishableDecreaseQuantityBig() throws StockException, InvalidItemException {
+		Item icecream = new PerishableItem(null, 0, 0, 0, 0, -12);
+		ItemStock is = stock.addNewItem(icecream, 40);	
+		stock.decreaseQuantity(is.getItemID(), 1000000);		
+	}
+
 }
